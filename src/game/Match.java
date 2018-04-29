7c4a50f6f39ff5c2284afc369cfaf67efc0bf20f
Aurélien Chevalier <aurelien.chevalier@isen.yncrea.fr>
2018-04-29 17:29:56 +0200

    Finalisation de l'organisation d'un match

--- src/game/Match.java
+++ src/game/Match.java
@@ -1,6 +1,5 @@
 package game;
 
-import java.util.List;
 import java.util.Random;
 
 public class Match {
@@ -19,6 +18,8 @@ public class Match {
   // Un générateur de nombre aléatoire
   private Random r = new Random();
 
+  // On stockera le nom du vainqueur
+  private Joueur vainqueur;
 
   public void jouerMatch() {
     // On va jouer des sets
@@ -27,23 +28,33 @@ public class Match {
     int setsB = 0 ;
     int nbSetMax = 2;
     if (inThreeSets) nbSetMax++;
-    while (setsA < nbSetMax || setsB < nbSetMax){
-      if (jouerSet() == joueurA) setsA++;
-      else setsB++;
+    while ((setsA < nbSetMax || setsB < nbSetMax) && vainqueur == null){
+      if (setsA == nbSetMax - 1 && setsB == nbSetMax - 1){
+        // Il s'agit du dernier set
+        if (jouerSet(true) == joueurA) setsA++;
+        else setsB++;
+      }
+      else {
+        if (jouerSet(false) == joueurA) setsA++;
+        else setsB++;
+      }
     }
     // TODO On met à jour le score des joueurs
   }
 
-  private Joueur jouerSet(){
+  private Joueur jouerSet(boolean isFinalSet){
     // Un set est composé de 6 jeux, ou un tie-break en cas de 6-6
     int jeuxA = 0;
     int jeuxB = 0;
     while (Math.abs(jeuxB-jeuxA) < 2 || (jeuxA < 6 && jeuxB < 6)){
-      if (jeuxA == 6 && jeuxB == 6){ // TODO condition différente pour dernier set
+      // On retourne tout de suite si on a un vainqueur.
+      if (vainqueur != null) return vainqueur;
+
+      if (jeuxA == 6 && jeuxB == 6 && !isFinalSet){ // Si dernier set il n'y aura pas de tie-break
         // Il s'agit d'un tie-break
-        return jouerTieBreak();
+        return jouerJeu(true); // le vainqueur du tie-break remporte le set
       } else {
-        if (jouerJeu() == joueurA) jeuxA++;
+        if (jouerJeu(false) == joueurA) jeuxA++;
         else jeuxB++;
       }
     }
@@ -51,16 +62,28 @@ public class Match {
     else return joueurB; // On retourne le joueur vainqueur
   }
 
-  private Joueur jouerJeu(){
+  private Joueur jouerJeu(boolean isTieBreak){
     int scoreA = 0;
     int scoreB = 0;
-    while (Math.abs(scoreB-scoreA) < 2 || (scoreA < 4 && scoreB < 4) ){
+    int scoreAObtenir = 4;
+    if (isTieBreak)
+      scoreAObtenir += 3; // On est dans le cas d'un tie break.
+    while (Math.abs(scoreB-scoreA) < 2 || (scoreA < scoreAObtenir && scoreB < scoreAObtenir) ){
       // On tire les valeurs des joueurs
       float valA = joueurA.getPuissance()/(100-enduA)*r.nextInt(100);
       float valB = joueurB.getPuissance()/(100-enduB)*r.nextInt(100);
       enduA -= joueurA.getPuissance()/100;
       enduB -= joueurB.getPuissance()/100;
-      // TODO abandon
+      // On détermine si un joueur veut abandonner le match
+      if (enduA <= 0){
+        this.abandon('a');
+        return joueurB;
+      }
+      if (enduB <= 0) {
+        this.abandon('b');
+        return joueurA;
+      }
+      // On détermine le vainqueur du round
       if (valA > valB) scoreA++;
       else scoreB++;
     }
@@ -68,23 +91,16 @@ public class Match {
     else return joueurB;
   }
 
-  private Joueur jouerTieBreak(){ // TODO factorize me
-    int scoreA = 0;
-    int scoreB = 0;
-    while (Math.abs(scoreB-scoreA) < 2 || (scoreA < 7 && scoreB < 7) ){
-      // On tire les valeurs des joueurs
-      float valA = joueurA.getPuissance()/(100-enduA)*r.nextInt(100);
-      float valB = joueurB.getPuissance()/(100-enduB)*r.nextInt(100);
-      enduA -= joueurA.getPuissance()/100;
-      enduB -= joueurB.getPuissance()/100;
-      // TODO abandon
-      if (valA > valB) scoreA++;
-      else scoreB++;
+  private void abandon(char joueur){
+    switch (joueur) {
+      case 'a':
+        this.setVainqueur(joueurB);
+        break;
+      case 'b':
+        this.setVainqueur(joueurA);
+        break;
     }
-    if (scoreA > scoreB) return joueurA;
-    else return joueurB;
   }
-
 
   public Match(Joueur joueurA, Joueur joueurB) {
     this.joueurA = joueurA;
@@ -110,5 +126,16 @@ public class Match {
   public Joueur getJoueurB() {
     // Getter de joueurB
     return joueurB;
+  }
+
+  public Joueur getVainqueur() {
+    // Getter de vainqueur
+    return vainqueur;
+  }
+
+  private void setVainqueur(Joueur vainqueur) {
+    // Setter de vainqueur
+    this.vainqueur = vainqueur;
+
   }
 }
